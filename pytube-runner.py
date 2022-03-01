de211068c11c85b1c6d4164d7522f7992acd9531
Shiva <100504615+o-i-z-y-s@users.noreply.github.com>
2022-03-01 02:11:06 -0800

    Following an AttributeError, script no longer continues running after reinstalling pytube.

--- pytube-runner.py
+++ pytube-runner.py
@@ -1,13 +1,9 @@
 import time
 import subprocess
 from pytube import YouTube
-from pytube.exceptions import RegexMatchError, VideoUnavailable, MembersOnly, VideoRegionBlocked, VideoPrivate, LiveStreamError, RecordingUnavailable, PyTubeError
+from pytube.exceptions import RegexMatchError, VideoUnavailable, MembersOnly, VideoRegionBlocked, VideoPrivate, LiveStreamError, RecordingUnavailable
 
 class PytubeRunner:
-    
-  def __init__(self):
-    self.hadAnAttributeError = False
-    
 
   def close(self):
     print("\nClosing now.", end=" ")
@@ -16,7 +12,6 @@ class PytubeRunner:
       print(str(i)+"...", end=" ")
     time.sleep(0.5)
 
-
   def run(self):
     url = input('> Paste a YouTube video url here: ')
 
@@ -24,9 +19,9 @@ class PytubeRunner:
       start = time.time()
 
       # A few notes about this next line (where the magic happens):
-      ## 'progressive=True' will always save audio and video in one file, but will only go up to 720p. 'adaptive=True' will download higher resolutions than 'progressive=True', but I don't guarantee it will have both video and audio.
-      ## You can delete '.get_highest_resolution()' and set "resolution='360p'" (or whatever res) inside .filter().
-      ## 'output_path' can be anything you'd like! './' means 'where the script is', so the folder is created there.
+      ## 'progressive=True' will always save audio and video in one file, but will only go up to 720p. 'adaptive=True' will download higher resolutions than 'progressive=True', but I don't guarantee it will have both video and audio in one file.
+      ## You can delete '.get_highest_resolution()' and set "resolution='360p'" (or whatever resolution you like) inside '.filter()'.
+      ## 'output_path' can be anything you'd like! './' creates the 'Ripped Videos' folder wherever the script file is.
       YouTube("\'"+url+"\'").streams.filter(progressive=True,file_extension='mp4').get_highest_resolution().download(output_path="./Ripped Videos/")
 
       end = time.time()
@@ -34,24 +29,20 @@ class PytubeRunner:
       self.run()
 
     except AttributeError:
-      if self.hadAnAttributeError == False:
-        print("\nAttribute Error! Two things can cause this to happen:\n1) You modified the params in .filter and they're failing.\n2) YouTube broke pytube again, so you need to reinstall it.")
+      print("\nAttribute Error! Two things can cause this to happen:\n1) You modified the params in .filter() and they're failing.\n2) YouTube broke pytube again, so you need to reinstall it or wait for a fixed version.")
 
-        confirmation = input("\n> Reinstall pytube now? [y/n]: ")
-        if confirmation.lower() == "y":
-          print("")
+      confirmation = input("\n> Reinstall pytube now? [y/n]: ")
+      if confirmation.lower() == "y":
+        print("")
 
-          reinstall = subprocess.Popen('pip install --upgrade --force-reinstall pytube', start_new_session=True)
-          reinstall.wait()
+        reinstall = subprocess.Popen('pip install --upgrade --force-reinstall pytube', start_new_session=True)
+        reinstall.wait()
 
-          print("")
-          self.hadAnAttributeError = True
-          self.run()
-        else:
-          self.close()
-      else:
-        print("\nLooks like the problem is in .filter, please fix and re-run.")
-        self.close()
+        print("\nPlease relaunch the script.")
+      else: 
+        print("\nPlease relaunch after checking params and/or installing a newer version of pytube.")
+      
+      self.close()
     
     except RegexMatchError:
       print("Invalid link! Please try again.\n")
@@ -80,7 +71,6 @@ class PytubeRunner:
     except VideoUnavailable:
       print("The video is unavailable.")
       self.run()
-
 
 if __name__ == "__main__":
   runner = PytubeRunner()
